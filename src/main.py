28152d13f20eb35b2874168053be3fcb0d68cfbe
Shayan-Farooq <59742577+Shayan-Farooq@users.noreply.github.com>
2023-08-18 23:56:05 -0700

    Added maze to main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from mouse import Mouse
+from maze import generate_maze, display_maze
 
-micromouse = Mouse()
-micromouse.check_surroundings()
+if __name__ == "__main__":
+    mouse = Mouse()
+    mouse.check_surroundings()
 
+    maze = generate_maze(5)
+    display_maze(maze)
--- src/maze.py
+++ src/maze.py
@@ -1,9 +1,9 @@
-def maze() -> list[int]:
+def generate_maze(parameter: int) -> list[int]:
     """
     Customisable maze generator.
 
     Specification:
-    - 16x16 grid
+    - 16x16 grid (or can be made general)
     - Defined start and endpoints
     - Parameter(s) to control complexity
     - Parameter(s) to control diagonals
@@ -15,4 +15,23 @@ def maze() -> list[int]:
     """
 
     # TODO: Implement maze generating algorithm.
-    pass
+    return [parameter, parameter, parameter]
+
+
+def display_maze(maze: list[int]) -> None:
+    """
+    Customisable maze displayer.
+
+    Specification:
+    - 16x16 grid (or can be made general)
+    - Defined start and end points
+    - Parameters to control features (walls, start, end)
+
+    TODO: Finish specification.
+
+    TODO: Add parameters.
+    TODO: Document subroutine.
+    """
+
+    # TODO: Implement subroutine.
+    print(maze)
