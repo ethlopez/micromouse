7253c10908685bbaa235353fbee34bb9b4b280ce
Shayan-Farooq <59742577+Shayan-Farooq@users.noreply.github.com>
2023-08-18 23:42:25 -0700

    Added and setup maze.py

---
+++ src/maze.py
@@ -0,0 +1,18 @@
+def maze() -> list[int]:
+    """
+    Customisable maze generator.
+
+    Specification:
+    - 16x16 grid
+    - Defined start and endpoints
+    - Parameter(s) to control complexity
+    - Parameter(s) to control diagonals
+
+    TODO: Finish specification.
+
+    TODO: Add parameters.
+    TODO: Document maze generating algorithm.
+    """
+
+    # TODO: Implement maze generating algorithm.
+    pass
